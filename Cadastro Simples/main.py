7e0266c5534d45c8ca15a5b248894fb5be8048a8
Fernando Alves <lfalvespe@gmail.com>
2022-07-28 08:27:10 -0300

    Add files via upload

---
+++ Cadastro Simples/main.py
@@ -0,0 +1,29 @@
+from uteis.dados import *
+from uteis.arquivo import *
+
+from time import sleep
+
+arq = 'pessoas.txt'
+
+if not arquivoExiste(arq):  
+  criarArquivo(arq)
+
+while True:
+  resposta = menu(['Mostrar lista de pessoas',
+     'Cadastrar nova pessoa',
+     'Sair'])
+  
+  if resposta == 1:
+    # Opção de listar o conteúdo de um arquivo
+    lerArquivo(arq)
+  elif resposta == 2:
+    cabecalho('Novo Cadastro')
+    nome = str(input('Nome: '))
+    idade = leiaint('Idade: ')
+    cadastrar(arq, nome, idade )
+  elif resposta == 3:
+    cabecalho('Saindo do sistema...Até logo!')
+    break
+  else:
+    print('\033[31mErro: Digite uma opção válida!\033[m')
+  sleep(1.5)
